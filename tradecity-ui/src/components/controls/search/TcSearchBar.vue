e140fcc33dc6ac4e075ff4e22090018e53d0a8d3
idbauman <jan.baumann@stud.hslu.ch>
2023-04-13 15:43:14 +0200

    frontend: search bar

--- tradecity-ui/src/components/controls/search/TcSearchBar.vue
+++ tradecity-ui/src/components/controls/search/TcSearchBar.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="flex bg-primary p-0.5 rounded-[3px] text-sm text-neutral-100">
-    <input v-model="text" placeholder="Search" type="text" class="bg-primary outline-none mr-3 ml-1 pt-0.5">
+    <input v-model="text" placeholder="Search" type="text" :class="['bg-primary outline-none mr-3 ml-1 pt-0.5 transition-width duration-400', text.length === 0 ? 'w-32' : 'w-72']">
     <img v-if="text.length === 0" class="ml-auto mr-1" src="../../../assets/img/icons/search.svg" alt="Search Icon">
     <img @click="resetText" v-else class="ml-auto mr-1 hover:cursor-pointer" src="../../../assets/img/icons/close.svg" alt="Close Icon">
 </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 
 const text = ref("");
 const resetText = () => text.value = "";
