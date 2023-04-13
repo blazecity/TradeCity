3ab36efd46db8f0ed91de778202cd4794d3b6ca5
idbauman <jan.baumann@stud.hslu.ch>
2023-04-13 15:41:43 +0200

    frontend: badge

---
+++ tradecity-ui/src/components/controls/badge/TcBadge.vue
@@ -0,0 +1,15 @@
+<template>
+<div :class="['font-bold rounded-sm text-sm py-0.5 px-1.5 m-1 inline-block', childClass, fontColorWhite ? 'text-white' : 'text-black']">{{ text }}</div>
+</template>
+
+<script setup lang="ts">
+interface TcBadgeProps {
+    text: string,
+    childClass: string,
+    fontColorWhite?: boolean
+}
+withDefaults(defineProps<TcBadgeProps>(), {
+    fontColorWhite: true
+});
+
+</script>
