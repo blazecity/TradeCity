8f61cf0b5caf1130c45d2bc589c1dcc35aff23d7
idbauman <jan.baumann@stud.hslu.ch>
2023-04-20 05:30:27 +0200

    change number input to combination of string and number input

--- tradecity-ui/src/components/controls/input/TcInput.vue
+++ tradecity-ui/src/components/controls/input/TcInput.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="flex items-center justify-between rounded bg-primary w-56">
-    <input @focusout="focusOut" :value="displayedValue" :placeholder="placeholder" type="text" class="bg-primary w-full outline-none m-1 text-xs" :title="placeholder" autocomplete="new-password">
+    <input @focusout="focusOut" :value="displayedValue" :placeholder="placeholder" type="text" class="general-input" :title="placeholder" autocomplete="new-password">
     <clickable-icon @click="clearValue">
         <close-icon :height="12" :width="12" styles="fill-white mr-1" />
     </clickable-icon>
@@ -13,27 +13,36 @@ import { ref } from 'vue';
 import CloseIcon from '../../icons/CloseIcon.vue';
 import ClickableIcon from '../../icons/ClickableIcon.vue';
 
-interface TcNumberInputProps {
+interface TcInputProps {
     placeholder: string,
     styles?: string,
-    defaultValue?: number
+    defaultValue?: number | string,
+    type?: "string" | "number"
 }
 
-const props = defineProps<TcNumberInputProps>();
+const props = withDefaults(defineProps<TcInputProps>(), {
+    type: "number"
+});
 
 interface TcInputEmits {
-    (event: "input", id: number): void
+    (event: "numberInput", value: number): void;
+    (event: "textInput", value: string): void;
 }
 
 const emit = defineEmits<TcInputEmits>();
 const inputValue = ref<string | number | undefined>(props.defaultValue);
 const displayedValue = computed(() => {
+    if (props.type === "string" && inputValue.value) {
+        emit("textInput", inputValue.value.toString());
+        return inputValue.value;
+    }
+
     if (!inputValue.value) {
-        emit("input", NaN);
+        emit("numberInput", NaN);
         return "";
     }
     const num = +inputValue.value.toString().replace(/[k]/g, "000").replace(/[m]/g, "000000").replace(/[^0-9.-]/g, "");
-    emit("input", num);
+    emit("numberInput", num);
     return Intl.NumberFormat("de-CH").format(num);
 });
 
--- tradecity-ui/src/components/views/TcMainView.vue
+++ tradecity-ui/src/components/views/TcMainView.vue
@@ -5,7 +5,6 @@
       <tc-header></tc-header>
       <main class="grid grid-cols-6 grid-rows-6 gap-1">
         <tc-panel title="CHF Bond Pricing Panel" :cols="6" :rows="2">
-          <tc-number-input placeholder="Input" @input="func" />
         </tc-panel>
         <tc-panel title="Panel 2" :cols="3" :rows="2">Content 2</tc-panel>
         <tc-panel title="Panel 2" :cols="3" :rows="2">Content 3</tc-panel>
@@ -20,12 +19,7 @@
 import TcNavigation from '../nav/TcNavigation.vue';
 import TcHeader from '../header/TcHeader.vue';
 import TcPanel from '../controls/panel/TcPanel.vue';
-import TcNumberInput from '../controls/input/TcNumberInput.vue';
-import { ref } from 'vue';
-
-function func(id: number): void {
-  console.log(id);
-}
+import TcInput from '../controls/input/TcInput.vue';
 </script>
 
 <style scoped>
