f8ca2c93a826ccd2fcff2f1f7ed1427570ab7cb6
Jan Baumann <janbaumann@mbp-jb.home>
2023-11-18 19:43:20 +0100

    solidjs project skeleton and badge element

---
+++ tradecity-ui/src/App.tsx
@@ -0,0 +1,11 @@
+import TcBadge from "./components/ui/badge/TcBadge.tsx";
+
+function App() {
+  return (
+    <>
+        <TcBadge text="Example" class="bg-yellow-300"></TcBadge>
+    </>
+  )
+}
+
+export default App;
---
+++ tradecity-ui/src/components/ui/badge/TcBadge.tsx
@@ -0,0 +1,15 @@
+import {JSX, splitProps} from "solid-js";
+
+interface TcBadgeProps extends JSX.HTMLAttributes<HTMLDivElement> {
+    text: string;
+}
+
+export default function TcBadge(props: TcBadgeProps) {
+    const [local, divProps] = splitProps(props, ['text']);
+
+    return (
+        <>
+            <div {...divProps} class={divProps.class + " text-xs inline p-1 rounded-sm"}>{ local.text }</div>
+        </>
+    )
+}
